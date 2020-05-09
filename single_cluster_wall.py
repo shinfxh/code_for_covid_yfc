f385e3a1d8b70e1d7fed49daf60b9be2c25a3808
Yongao <38068386+yong-ao@users.noreply.github.com>
2020-05-09 12:25:16 +0800

    delete

--- single_cluster_wall.py
+++ single_cluster_wall.py
@@ -7,11 +7,9 @@ n=100;
 width=50;
 height=50;
 r=5;
-xrange=100;
-yrange=100;
 x_vel=[0 for i in range(n)];
 y_vel=[0 for i in range(n)];
-cluster=[[x_center+random.randrange(-xrange, xrange), y_center+random.randrange(-yrange, yrange)] for i in range(n)];
+cluster=[[x_center+random.randrange(-100, 100), y_center+random.randrange(-100, 100)] for i in range(n)];
 
 
 x_vel=50;
@@ -21,7 +19,12 @@ dt=0.1;
 x_acc=10;
 y_acc=10;
 
+x_bias=10;
+y_bias=10;
 
+vel=[[random.uniform(-x_vel, x_vel), random.uniform(-y_vel, y_vel)] for i in range(n)];
+cluster=np.array(cluster);
+vel=np.array(vel);
 
 import pygame
 pygame.init()
@@ -30,18 +33,25 @@ win = pygame.display.set_mode((500,500));
 
 run=True;
 while run:
-    vel=[[random.uniform(-x_vel, x_vel), random.uniform(-y_vel, y_vel)] for i in range(n)];
-    cluster=np.array(cluster);
-    vel=np.array(vel);
-    
     win.fill((0,0,0));
     pygame.time.delay(int(dt*1000));
     
+    acc=[[random.uniform(-x_acc, x_acc), random.uniform(-y_acc, y_acc)] for i in range(n)];
+    acc=np.array(acc);
     
-    cluster1=np.add(cluster, vel*dt);
-    cluster2=np.add(cluster,-vel*dt);
-    cluster=np.where(cluster<500,cluster1,cluster2);
-    cluster=np.where(cluster>0,cluster,cluster2);
+    
+    vel=np.add(vel, acc*dt);
+    
+    for i in range(n):
+        [x_pos, y_pos]=cluster[i];
+        if x_pos<0 or x_pos>500:
+            vel[i,0]*=-1;
+        if y_pos<0 or y_pos>500:
+            vel[i,1]*=-1;
+        
+        
+    
+    cluster=np.add(cluster, vel*dt);
     cluster_rounded=np.rint(cluster);
     
     for event in pygame.event.get():
